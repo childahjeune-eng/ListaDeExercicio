c2a15ec99effc1c7d54dc5f1fa4a0ff300c1233e
childah.Al.jeune <childah.jeune@escola.pr.gov.br>
2026-02-26 14:54:52 +0000

    lista de exercicios

---
+++ lista1.js
@@ -0,0 +1,29 @@
+//criar um programa para exibir a mensagem: "olá, mundo." 
+let mensagem = 'olá, mundo'
+console.log(mensagem)
+
+//criar um programa para exibir seu nome completo
+let nome = 'childah Al drielys jeune'
+console.log(nome)
+
+//cria um programa que mostre em número e exiba seu dobro
+let num = 9 
+ dobro = num*2
+console.log(dobro)
+
+//criar um programa que mostre em número e exiba seu triplo
+let num2 = 6
+ triplo = num*3
+console.log(triplo)
+
+//criar um programa que mostre duas notas e calcule a média
+let num3 = '5,9'
+let num4 = '7,8'
+'calcule a média de' = '5,9 * 7,8'
+console.log(calcule)
+
+//criar um programa para converter metros em centimetros
+//criar um programa para mostrar o antecessor de um número
+//criar um programa que mostre a área de um retãngulo
+//criar um programa para calcular um aumento de 10% num salário de 2000,00
+//criar um programa para verificar se eu número é par ou impar
