bcb0be41f47d5fddc5980f19b05df1c3b8ac21fa
childah.Al.jeune <childah.jeune@escola.pr.gov.br>
2026-02-27 12:34:48 +0000

    childah

--- lista1.js
+++ lista1.js
@@ -17,13 +17,38 @@ let num2 = 6
 console.log(triplo)
 
 //criar um programa que mostre duas notas e calcule a média
-let num3 = '5,9'
-let num4 = '7,8'
-'calcule a média de' = '5,9 * 7,8'
-console.log(calcule)
+let nota1 = 5.9;
+let nota2 = 7.8;
+let media = (nota1 + nota2) / 2;
+console.log("Média:", media);
 
 //criar um programa para converter metros em centimetros
+let metros = 3;
+let centimetros = metros * 100;
+console.log("Centímetros:", centimetros);
+
 //criar um programa para mostrar o antecessor de um número
+let numero = 10;
+let antecessor = numero - 1;
+console.log("Antecessor:", antecessor);
+
 //criar um programa que mostre a área de um retãngulo
+let base = 5;
+let altura = 4;
+let area = base * altura;
+console.log("Área do retângulo:", area);
+
 //criar um programa para calcular um aumento de 10% num salário de 2000,00
+let salario = 2000;
+let aumento = salario * 0.10;
+let novoSalario = salario + aumento;
+console.log("Novo salário:", novoSalario);
+
 //criar um programa para verificar se eu número é par ou impar
+let valor = 7;
+
+if (valor % 2 === 0) {
+    console.log("É par");
+} else {
+    console.log("É ímpar");
+}
---
+++ lista2.js
@@ -0,0 +1,23 @@
+//criar um programa que mostre se um numero e positivo ou negativo
+let num1 = 99
+if (num1>=0) {
+    console.log (`o numero ${num1} e positivo`)
+} else {
+    console.log (`o numero ${num1} e negativo`)
+}
+
+//criar um programa que mostre se um ano bissexto.
+let ano = 1984
+if (ano % 4 === 0) {
+    console.log ('bissexto');
+} else {
+    console.log ('não e bissexto.')
+}
+
+//criar um programa que mostre uma senha e a mensagem acesso permitido
+let senha = "0508chil"
+if (senha === "0508chil") {
+    console.log ("acesso permitido")
+} else {
+    console.log ("acesso negado")
+}
